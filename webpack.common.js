163cf98d6e33c180c8269ee0fb8a43ae37b3b27e
xavigimenezsanchez <javiergimenezsanchez@gmail.com>
2022-03-22 18:54:23 +0100

    Added webpack and react

---
+++ webpack.common.js
@@ -0,0 +1,66 @@
+import sass from "sass";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import CopyPlugin from "copy-webpack-plugin";
+import { resolve, join } from "path";
+const __dirname = resolve();
+
+export default {
+  entry: "./index.tsx",
+  watchOptions: {
+    ignored: ["**/node_modules", "**/server/"],
+  },
+  output: {
+    filename: "./app-bundle.js",
+    path: resolve(`${__dirname}/server/public`, "dist"),
+    clean: true,
+  },
+  resolve: {
+    extensions: [".Webpack.js", ".web.js", ".ts", ".js", ".jsx", ".tsx"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          "style-loader",
+          "css-loader",
+          {
+            loader: "sass-loader",
+            options: {
+              // Prefer `dart-sass`
+              implementation: sass,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(png|svg)$/i,
+        use: ["url-loader"],
+      },
+      // {
+      //   test: /\.(eot|ttf|woff)$/i,
+      //   use: ["file-loader"],
+      // },
+    ],
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        { from: "server/public/manifest.json" },
+        { from: "server/public/favicon.ico" },
+        { from: "server/public/logo.png" },
+      ],
+    }),
+    new HtmlWebpackPlugin({
+      inject: false,
+      template: join(__dirname, "./server/public/index.html"),
+    }),
+  ],
+};
---
+++ webpack.dev.js
@@ -0,0 +1,24 @@
+import { merge } from "webpack-merge";
+import common from "./webpack.common.js";
+
+export default merge(common, {
+  mode: "development",
+  devtool: "inline-source-map",
+  devServer: {
+    static: "./server/public/dist",
+    historyApiFallback: true,
+    host: "0.0.0.0",
+    compress: true,
+    hot: true,
+    port: 3001,
+
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+    },
+    proxy: {
+      context: ["/api"],
+      target: "http://localhost:3000",
+      secure: false,
+    },
+  },
+});
