6b2b12056118b2e71d46ce35db0d284e8462065e
Xavier Giménez Sánchez <xavierg@theprojectgroup.com>
2022-03-26 22:48:49 +0100

    added more package.json scrips

--- server/index.js
+++ server/index.js
@@ -7,12 +7,17 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 const __dirname = resolve();
-var exposedFileSystem = join(__dirname, "exposedFileSystem");
-app.use(express.static(exposedFileSystem));
+// var exposedFileSystem = join(__dirname, "exposedFileSystem");
+// app.use(express.static(exposedFileSystem));
+
+var staticPath = join(__dirname, "server/public/dist");
+app.use(express.static(staticPath));
 
 app.get("/api/dir", dir);
 app.get("/api/get", getFile);
-(req, res) => dir(res, req.bodyres.send("dir" + JSON.stringify(req.body)));
+const DIST_DIR = join(__dirname, "./server/public/dist");
+const HTML_FILE = join(DIST_DIR, "index.html");
+app.get("/*", (req, res) => res.sendFile(HTML_FILE));
 
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
